388a3f15e30af490cc639a468aaaf986b34fa787
Jerry Yarnetsky <yarnete@miamioh.edu>
2026-02-17 14:19:24 -0500

    changing script link for libchat

--- 01OHIOLINK_MU-MU_NDE26/assets/js/custom.js
+++ 01OHIOLINK_MU-MU_NDE26/assets/js/custom.js
@@ -12,7 +12,7 @@
     lc.async = 'true';
     lc.src =
       ('https:' == document.location.protocol ? 'https://' : 'http://') +
-      'v2.libanswers.com/load_chat.php?hash=48597dfc1078556815ee78d6e99e7a7b';
+      'libanswers.lib.miamioh.edu/load_chat.php?hash=48597dfc1078556815ee78d6e99e7a7b';
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(lc, s);
   })();
