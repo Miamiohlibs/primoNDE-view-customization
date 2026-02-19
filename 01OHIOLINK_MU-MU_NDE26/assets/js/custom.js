1146f6bd70a10ef5eb07f3cd0407bea220a42e3f
Jerry Yarnetsky <yarnete@miamioh.edu>
2026-02-19 15:21:50 -0500

    update LibCaht location

--- 01OHIOLINK_MU-MU_NDE26/assets/js/custom.js
+++ 01OHIOLINK_MU-MU_NDE26/assets/js/custom.js
@@ -9,14 +9,10 @@
     lc.async = 'true';
     lc.src =
       ('https:' == document.location.protocol ? 'https://' : 'http://') +
-      'libanswers.lib.miamioh.edu/load_chat.php?hash=48597dfc1078556815ee78d6e99e7a7b';
+      'libanswers.lib.miamioh.edu/load_chat.php?hash=b81f60084589f2d6a37b0cf4420a7a9d11eb67b362a45685ae5b79b41005310d';
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(lc, s);
   })();
   // End the chat button
   
 })();
-
-
-
- 
